bdb8dec712ba0bad72ff8eac3cad0729dd646123
Jonathan Bredin <bredin@acm.org>
2022-03-24 11:59:16 -0500

    porting sqlite ops to typescript

--- src/timekeeper.ts
+++ src/timekeeper.ts
@@ -5,6 +5,24 @@ import { Database, open } from 'sqlite';
 const debug = Debug('rsvp:timekeeper');
 const error = Debug('rsvp:timekeeper:error');
 
+export interface DateTime {
+  attend?: number,
+  duration: string,
+  event: number,
+  hhmm: string,
+  id: number,
+  yyyymmdd: string
+}
+
+export interface Event {
+  dateTime: number,
+  dateTimes: Array<DateTime>,
+  description: string,
+  id: number,
+  name: string,
+  venue: number
+}
+
 export interface ParticipantOptions {
   email?: string,
   id?: number,
@@ -22,20 +40,45 @@ export interface Venue {
   address: string
 }
 
+function simpleWhere(query: any): string {
+  if (!query) {
+    return '';
+  }
+
+  const entries = Object.entries(query);
+  if (!entries.length) {
+    return '';
+  }
+
+  return `WHERE ${entries.map((e) => {
+    const [key, arg] = e;
+    if (key === 'id' || key.endsWith('Id')) {
+      return `${key}=${parseInt(arg as string, 10)}`;
+    }
+
+    return `${key} LIKE '%${q(arg as string)}%'`;
+  }).
+    join(' AND ')}`;
+}
+
+function q(str: string): string {
+  return str.replace(/'/g, '\'\'');
+}
+
 export function validateYyyyMmDd(yyyymmdd: string): void {
-  if (!yyyymmdd?.match(/^[0-9]{4}[/-][0-9]{1,2}[/-][0-9]{1,2}$/)) {
+  if (!yyyymmdd.match(/^[0-9]{4}[/-][0-9]{1,2}[/-][0-9]{1,2}$/)) {
     throw new Error(`yyyymmdd validation: ${yyyymmdd}`);
   }
 }
 
 export function validateHhMm(hhmm: string): void {
-  if (!hhmm?.match(/^[0-9]{1,2}:[0-9]{2}$/)) {
+  if (!hhmm.match(/^[0-9]{1,2}:[0-9]{2}$/)) {
     throw new Error(`hhmm validation: ${hhmm}`);
   }
 }
 
 export function validateDuration(duration: string): void {
-  if (!duration?.match(/^[0-9]+m$/)) {
+  if (!duration.match(/^[0-9]+m$/)) {
     throw new Error(`duration validation: ${duration}`);
   }
 }
@@ -47,6 +90,29 @@ export class TimeKeeper {
     this.config = {
       dbFilename: opts.dbFilename
     };
+  }
+
+  /**
+   * Set the date time for the event.
+   */
+  async closeEvent(eventId: number, dateTimeId: number, cachedDb?: Database):
+    Promise<void> {
+    let query;
+    if (dateTimeId > 0) {
+      query = 'UPDATE events SET dateTime = :dateTimeId WHERE rowid = :eventId';
+    } else {
+      query = 'UPDATE events SET dateTime = -1 WHERE rowid = :eventId';
+    }
+    const queryOpts = {
+      ':dateTimeId': dateTimeId,
+      ':eventId': eventId,
+    };
+    debug('closeEvent', query);
+    const db = cachedDb || await this.openDb();
+    db.run(query, queryOpts);
+    if (!cachedDb) {
+      await db.close();
+    }
   }
 
   /**
@@ -70,7 +136,7 @@ export class TimeKeeper {
       ':hhmm': hhmm,
       ':duration': duration
     });
-    const lastId = await result.lastID || 0;
+    const lastId = result.lastID || 0;
     if (lastId <= 0) {
       throw new Error(`Unchecked error createDateTime: ${query} :${yyyymmdd} :${hhmm} :${duration}`);
     }
@@ -183,6 +249,231 @@ export class TimeKeeper {
     }
   }
 
+  /**
+    * @return a promise to dateTime info.
+    */
+  async getDateTime(dateTimeId: number, cachedDb?: Database):
+    Promise<DateTime | undefined> {
+    const query = 'SELECT rowid AS id, event, yyyymmdd, hhmm, duration FROM dateTimes where id=?';
+    debug('getDateTime', query);
+
+    const db = cachedDb || await this.openDb();
+    try {
+      return await db.get(query, dateTimeId);
+    } finally {
+      if (!cachedDb) {
+        await db.close();
+      }
+    }
+  }
+
+  /**
+   * @return promise of event object description.
+   * @param eventId
+   * @param userIdOpt if specified, join the relevant rsvps to the
+   *   associated datetimes.
+   */
+  async getEvent(eventId: number, userIdOpt?: number, cachedDb?: Database):
+    Promise<Event | undefined> {
+    const eventQuery =
+      'SELECT rowid AS id, name, description, venue, dateTime FROM events WHERE id=?';
+    debug('getEvent', eventQuery);
+
+    const db = cachedDb || await this.openDb();
+    try {
+      const event = await db.get(eventQuery, eventId);
+      if (!event) {
+        return undefined;
+      }
+
+      // Put the datetimes on the event.
+      const dtQuery = userIdOpt ?
+        'SELECT dt.rowid AS id, dt.*, r.attend ' +
+        'FROM dateTimes dt ' +
+        `LEFT JOIN (SELECT * FROM rsvps WHERE participant = :userId) r ` +
+        'ON dt.rowid = r.dateTime ' +
+        'WHERE dt.event = :eventId' :
+        'SELECT rowid AS id, * FROM dateTimes WHERE event = :eventId';
+      const dtQueryOpts = {
+        ':eventId': eventId,
+        ':userId': userIdOpt
+      };
+      debug('getEvent dt', dtQuery, userIdOpt, dtQuery);
+      event.dateTimes = await db.all(dtQuery, dtQueryOpts);
+      // TODO: clean up
+      if (event.dateTime) {  // replace chosen dateTime id with the object
+        event.dateTime = event.dateTimes.
+          find((dt: DateTime) => dt.id === event.dateTime);
+      } else {
+        event.dateTime = undefined;
+      }
+      return event;
+    } finally {
+      if (!cachedDb) {
+        await db.close();
+      }
+    }
+  }
+
+  /**
+   * @param opts venue-query
+   * @return promise an array of event ids.
+   */
+  async getEvents(opts: string, cachedDb?: Database):
+    Promise<Array<number>> {
+    const query = `SELECT rowid AS id FROM events ${simpleWhere(opts)}`;
+    debug('getEvents', query);
+    const db = cachedDb || await this.openDb();
+    try {
+      return db.all(query).
+        then((result) => result.map((x) => x.id));
+    } finally {
+      if (!cachedDb) {
+        await db.close();
+      }
+    }
+  }
+
+  async getNevers(participantId: number, sinceOpt?: string, cachedDb?: Database):
+    Promise<Array<string>> {
+    let since;
+    if (sinceOpt) {
+      since = ` AND yyyymmdd > '${sinceOpt}'`;
+    } else {
+      since = '';
+    }
+
+    const query = 'SELECT * FROM nevers ' +
+      `WHERE participant=${participantId}${since}`;
+    debug('getNevers', query);
+    const db = cachedDb || await this.openDb();
+    try {
+      return db.all(query).
+        then((result) => result.map((row) => row.yyyymmdd));
+    } finally {
+      if (!cachedDb) {
+        await db.close();
+      }
+    }
+  }
+
+  /**
+   * @return a promise to a map of datetimes to responses.
+   */
+  async getRsvps(eventId: number, userId: number, cachedDb?: Database):
+    Promise<Map<number, number>> {
+
+    const query = 'SELECT datetime, attend FROM rsvps ' +
+      'WHERE event = :eventId AND participant = :userId';
+    const queryOpts = {
+      ':eventId': eventId,
+      ':userId': userId
+    };
+    debug('getRsvps', eventId, userId);
+
+    const db = cachedDb || await this.openDb();
+    try {
+      return db.all(query, queryOpts).
+        then((result) => result.reduce(
+          (accum, x) => {
+            accum[x.dateTime] = x.attend; // eslint-disable-line
+            return accum;
+          },
+          {},
+        ));
+    } finally {
+      if (!cachedDb) {
+        await db.close();
+      }
+    }
+  }
+
+  /**
+   * @return promise to id.
+   */
+  async getUserId(userName: string, cachedDb?: Database): Promise<number> {
+    const query = `SELECT rowid FROM participants WHERE name = '${q(userName)}'`;
+    debug('getUserId', query);
+
+    const db = cachedDb || await this.openDb();
+    try {
+      return db.all(query).
+        then((result) => {
+          if (!result.length) {
+            return -1;
+          }
+          return result[0].rowid;
+        });
+    } finally {
+      if (!cachedDb) {
+        await db.close();
+      }
+    }
+  }
+
+  /**
+   * @return promise to id.
+   */
+  async getUserIdByEmail(email: string, cachedDb?: Database): Promise<number> {
+    const query = `SELECT rowid FROM participants WHERE email = '${q(email.trim())}'`;
+    debug('getUserIdByEmail', query);
+
+    const db = cachedDb || await this.openDb();
+    try {
+      return db.all(query).
+        then((result) => {
+          if (!result.length) {
+            return -1;
+          }
+          return result[0].rowid;
+        });
+    } finally {
+      if (!cachedDb) {
+        await db.close();
+      }
+    }
+  }
+
+  /**
+   * @return promise to info.
+   */
+  async getUserInfo(userId: number, cachedDb?: Database): Promise<ParticipantOptions | undefined> {
+    const query = `SELECT rowid as id, name, section, organizer, email FROM participants WHERE id = ${userId}`;
+    debug('getUserInfo', query);
+
+    const db = cachedDb || await this.openDb();
+    try {
+      return db.all(query).
+        then((result: Array<ParticipantOptions>) => {
+          if (!result.length) {
+            return undefined;
+          }
+          return result[0];
+        });
+    } finally {
+      if (!cachedDb) {
+        await db.close();
+      }
+    }
+  }
+
+  /**
+   * @return a promise to a key-value lookup.
+   */
+  async getValue(userId: number, key: string, cachedDb?: Database): Promise<string | number> {
+    const query = `SELECT value FROM key_value WHERE key='${q(key)}'`;
+    debug('getValue', query);
+    const db = cachedDb || await this.openDb();
+    try {
+      const [result] = await db.all(query);
+      return result && result.value;
+    } finally {
+      if (!cachedDb) {
+        await db.close();
+      }
+    }
+  }
+
   async getVenueByName(name: string, cachedDb?: Database): Promise<Venue | undefined> {
     const query = 'SELECT rowid, address FROM venues WHERE name=?';
     debug('getVenueByName', query, name);
@@ -200,11 +491,67 @@ export class TimeKeeper {
     }
   }
 
+  async never(participantId: number, dateStr: string, cachedDb?: Database): Promise<void> {
+    const ts = new Date().getTime();
+    const neverQuery = 'INSERT OR IGNORE INTO nevers(' +
+      'participant, yyyymmdd) VALUES' +
+      `(${participantId}, '${dateStr}')`;
+
+    const coincidentDts =
+      `SELECT event, rowid as dateTime, ${participantId} AS participant, -1, ${ts} AS timestamp
+        FROM dateTimes
+        WHERE yyyymmdd='${dateStr}'`;
+    const updateDTQuery =
+      `INSERT OR REPLACE INTO rsvps(event, dateTime, participant, attend, timestamp)
+        ${coincidentDts}`;
+
+    debug('never', neverQuery);
+    const db = cachedDb || await this.openDb();
+    await db.run(neverQuery);
+    debug('never update', updateDTQuery);
+    db.run(updateDTQuery);
+    if (!cachedDb) {
+      await db.close();
+    }
+  }
+
   async openDb(): Promise<Database> {
     return open<sqlite3.Database, sqlite3.Statement>({
       filename: this.config.dbFilename,
       driver: sqlite3.Database
     });
+  }
+
+  /**
+     * @return promise to unique response id.
+     */
+  async rsvp(eventId: number, participantId: number, dateTimeId: number, attend: number, cachedDb: Database):
+    Promise<number> {
+    const innerJoinId = '(SELECT rowid FROM rsvps WHERE ' +
+      'event=:eventId AND participant=:participantId ' +
+      'AND dateTime=:dateTimeId';
+    const ts = new Date().getTime();
+    const query = 'INSERT OR REPLACE INTO rsvps(' +
+      'rowid, event, participant, dateTime, attend, timestamp) VALUES' +
+      `(${innerJoinId}), :eventId, :participantId, :dateTimeId, :attend, :ts)`;
+    const queryOpts = {
+      ':eventId': eventId,
+      ':participantId': participantId,
+      ':dateTimeId': dateTimeId,
+      ':attend': attend,
+      ':ts': ts
+    };
+    debug('rsvp', query);
+    const db = cachedDb || await this.openDb();
+    try {
+      const result = await db.run(query, queryOpts);
+      const lastId = result.lastID || 0;
+      return lastId;
+    } finally {
+      if (!cachedDb) {
+        await db.close();
+      }
+    }
   }
 
   /**
--- test/timekeeper.test.ts
+++ test/timekeeper.test.ts
@@ -88,6 +88,84 @@ describe('Sqlite server tests', () => {
     await db.close();
   });
 
+  test('Retrieves an event', async () => {
+    const tk = newTimeKeeper();
+    const db = await tk.setup();
+    const eid = await tk.createEvent('Hula Contest', 17, 'Swing those hips!', db);
+
+    const e = await tk.getEvent(eid, undefined, db);
+    expect(e).toEqual({
+      dateTime: undefined,
+      dateTimes: [],
+      description: 'Swing those hips!',
+      id: eid,
+      name: 'Hula Contest',
+      venue: 17
+    });
+    await db.close();
+  });
+
+  test('Retrieves an event for a user', async () => {
+    const tk = newTimeKeeper();
+    const db = await tk.setup();
+    const vid = 17;
+    const pid = 19;
+    const eid = await tk.createEvent('Hula Contest', vid, 'Swing those hips!', db);
+    const did = await tk.createDateTime(eid, '2022-03-23', '16:31', '5m', db);
+    await tk.rsvp(eid, pid, did, 1, db);
+    const e = await tk.getEvent(eid, pid, db);
+    expect(e).toEqual({
+      dateTime: undefined,
+      dateTimes: [{
+        attend: 1,
+        duration: '5m',
+        event: eid,
+        hhmm: '16:31',
+        id: did,
+        yyyymmdd: '2022-03-23'
+      }],
+      description: 'Swing those hips!',
+      id: eid,
+      name: 'Hula Contest',
+      venue: 17
+    });
+    await db.close();
+  });
+
+  test('Retrieves an event with a selected time', async () => {
+    const tk = newTimeKeeper();
+    const db = await tk.setup();
+    const eid = await tk.createEvent('Hula Contest', 17, 'Swing those hips!', db);
+    const did = await tk.createDateTime(eid, '2022-03-23', '16:31', '5m', db);
+    await tk.closeEvent(eid, did, db);
+    const e = await tk.getEvent(eid, undefined, db);
+
+    const dt = {
+      duration: '5m',
+      event: eid,
+      hhmm: '16:31',
+      id: did,
+      yyyymmdd: '2022-03-23'
+    };
+    expect(e).toEqual({
+      dateTime: dt,
+      dateTimes: [dt],
+      description: 'Swing those hips!',
+      id: eid,
+      name: 'Hula Contest',
+      venue: 17
+    });
+    await db.close();
+  });
+
+  test('Queries for missing event', async () => {
+    const tk = newTimeKeeper();
+    const db = await tk.setup();
+    const e = await tk.getEvent(1, undefined, db);
+    expect(e).toBe(undefined);
+    await db.close();
+  });
+
   test('Creates a participant', async () => {
     const tk = newTimeKeeper();
     const db = await tk.setup();
@@ -109,6 +187,51 @@ describe('Sqlite server tests', () => {
     const db = await tk.setup();
     const did = await tk.createDateTime(99, '2022-03-23', '16:31', '5m', db);
     expect(did).toBe(1);
+    await db.close();
+  });
+
+  test('Retrieves a dateTime', async () => {
+    const tk = newTimeKeeper();
+    const db = await tk.setup();
+    const did = await tk.createDateTime(99, '2022-03-23', '16:31', '5m', db);
+    const dt = await tk.getDateTime(did, db);
+    expect(dt).toEqual({
+      id: did,
+      event: 99,
+      yyyymmdd: '2022-03-23',
+      hhmm: '16:31',
+      duration: '5m'
+    });
+    await db.close();
+  });
+
+  test('Gets never attend dates', async () => {
+    const tk = newTimeKeeper();
+    const db = await tk.setup();
+    const id = await tk.createParticipant('Bilbo', undefined, db);
+    await tk.never(id, '2012-01-01', db);
+    await tk.never(id, '2012-01-02', db);
+    const nevers = await tk.getNevers(id, undefined, db);
+    expect(nevers).toEqual(['2012-01-01', '2012-01-02']);
+
+    const recentNevers = await tk.getNevers(id, '2012-01-01', db);
+    expect(recentNevers).toEqual(['2012-01-02']);
+  });
+
+  test('Handles missing keys', async () => {
+    const tk = newTimeKeeper();
+    const db = await tk.setup();
+    expect(await tk.getValue(1, 'foo', db)).not.toBeDefined();
+    await db.close();
+  });
+
+  test('Retrieves values for keys', async () => {
+    const tk = newTimeKeeper();
+    const db = await tk.setup();
+    await db.run(
+      'INSERT INTO key_value(key, value) VALUES (\'foo\', \'bar\')',
+    );
+    expect(await tk.getValue(1, 'foo', db)).toEqual('bar');
     await db.close();
   });
 
